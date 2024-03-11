7dfe9015da0116c2de0807244d8992a92d36c022
Ravindranath Singh <ravindranathsingh391@gmail.com>
2024-03-11 21:41:20 +0530

    Added error handler function

---
+++ src/utils/validation-error.js
@@ -0,0 +1,22 @@
+const AppErrors = require('./error-handler')
+const { StatusCodes } = require('http-status-codes')
+
+class ValidationError extends AppErrors {
+    constructor(error) {
+        
+        let errorName = error.name;
+        let explanation = [];
+        error.errors.forEach((err) => {
+            explanation.push(err.message)
+        });
+
+        super (
+            errorName,
+            'Not able to validate the date sent in the request',
+            explanation,
+            StatusCodes.BAD_REQUEST
+        )
+    }
+}
+
+module.exports = ValidationError
